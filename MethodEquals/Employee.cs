3e3e047522babbb259f81618464c0f3c3a5830e2
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-12 10:42:57 +0300

    Добавьте файлы проекта.

---
+++ MethodEquals/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodEquals;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string PlaceOfWork { get; set; }
+    public Employee()
+    {
+        Name = "Jora";
+        SurName = "Normalnov";
+        PlaceOfWork = "Dom";
+    }
+    public Employee(string name, string surName, string placeOfWork)
+    {
+        Name = name;
+        SurName = surName;
+        PlaceOfWork = placeOfWork;
+    }
+    public override bool Equals(object? obj)
+    {
+        var other = obj as Employee;
+        if (other == null || obj == null)
+        {
+            return false;
+        }
+        else if (other.Name == Name && other.SurName == SurName && other.PlaceOfWork == PlaceOfWork)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ MethodEquals/Program.cs
@@ -0,0 +1,19 @@
+﻿using MethodEquals;
+
+Employee employee = new();
+Employee employee2 = new("Jora", "Normalnov", "Dom");
+void WorkEquals()
+{
+    if (employee.Equals(employee2))
+    {
+        Console.WriteLine("Данные объектов совпадают");
+    }
+    else
+    {
+        Console.WriteLine("Данные объектов не совпадают");
+    }
+}
+WorkEquals();
+
+
+
